069ab242b9d2beb43f1f6eacbca79b4acc216e1c
kota <52403688+yashikota@users.noreply.github.com>
2023-02-14 18:05:47 +0900

    fix width

--- python/jancode/main.py
+++ python/jancode/main.py
@@ -22,11 +22,11 @@ def generate(number: int | str, path: str = ".") -> None:
     """
     Generate JAN code
     """
-    length, width, jancode_number, center_position, combi, jancode_type = property.set_property(
+    length, jancode_number, center_position, combi, jancode_type = property.set_property(
         str(number)
     )
     check_digit(str(number), length)
     binary_number: str = convert.binary(jancode_number, combi, center_position)
 
     os.makedirs(path, exist_ok=True)
-    svg.create(binary_number, width, path, str(number), jancode_type)
+    svg.create(binary_number, len(binary_number), path, str(number), jancode_type)
--- python/jancode/property.py
+++ python/jancode/property.py
@@ -3,11 +3,10 @@ from jancode.characters import combination
 
 def set_property(number: str) -> None:
     """
-    Set the length, height, and width of the JAN code
+    Set the length and height of the JAN code
     """
     guard_bar_length: int = 11  # Guard bar length
     length: int = len(number)
-    width: int = int(length * 7 + guard_bar_length)
 
     if length == 13:
         # Subtract the first digit of the standard barcode because the standard barcode has 12 digits.
@@ -23,4 +22,4 @@ def set_property(number: str) -> None:
         )
         jancode_type = "short"
 
-    return length, width, jancode_number, center, combi, jancode_type
+    return length, jancode_number, center, combi, jancode_type
