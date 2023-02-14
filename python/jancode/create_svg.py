d5baa0997ea69634714af553f0eeea1b683796f3
kota <52403688+yashikota@users.noreply.github.com>
2023-02-14 17:47:40 +0900

    create svg

--- python/jancode/create_svg.py
+++ python/jancode/create_svg.py
@@ -1,7 +1,37 @@
-def create(binary_number: str, width: str, path: str, number: str):
-    content = [f'<rect x="{x}" y="0" width="1" height="90%" fill="#000000" />' for x, i in enumerate(binary_number) if int(i)]
+def create(binary_number: str, width: str, path: str, number: str, jancode_type: str):
+    content = list()
+    margin = 8
+    standard_guard_bar_point = [0, 1, 2, 45, 46, 47, 48, 49, 92, 93, 94]
+    short_guard_bar_point = [0, 1, 2, 31, 32, 33, 34, 35, 64, 65, 66]
 
-    meta_head = f'<svg version="1.1"\nbaseProfile="full"\nwidth="{width}"\nheight="80"\nxmlns="http://www.w3.org/2000/svg">\n\n' + \
+    for i, x in enumerate(binary_number, margin):
+        if(int(x)):
+            if jancode_type == "standard":
+                if i in [n + margin for n in standard_guard_bar_point]:
+                    content.append(f'<rect x="{i}" y="1" width="1" height="90%" fill="#000000" />')
+                else:
+                    content.append(f'<rect x="{i}" y="1" width="1" height="85%" fill="#000000" />')
+            else:
+                if i in [n + margin for n in short_guard_bar_point]:
+                    content.append(f'<rect x="{i}" y="1" width="1" height="90%" fill="#000000" />')
+                else:
+                    content.append(f'<rect x="{i}" y="1" width="1" height="85%" fill="#000000" />')
+
+    for i in range(len(number)):
+        if jancode_type == "standard":
+            if i == 0:
+                content.append(f'<text x="1" y="98%" font-size="12">{number[i]}</text>')
+            elif i < 7:
+                content.append(f'<text x="{i * 7 + 4}" y="98%" font-size="12">{number[i]}</text>')
+            else:
+                content.append(f'<text x="{i * 7 + 9}" y="98%" font-size="12">{number[i]}</text>')
+        else:
+            if i < 4:
+                content.append(f'<text x="{i * 7 + + 12}" y="98%" font-size="12">{number[i]}</text>')
+            else:
+                content.append(f'<text x="{i * 7 + 16}" y="98%" font-size="12">{number[i]}</text>')
+
+    meta_head = f'<svg version="1.1"\nbaseProfile="full"\nwidth="{margin + width + margin}"\nheight="80"\nxmlns="http://www.w3.org/2000/svg">\n\n' + \
     '<!-- fill background -->\n<rect width="100%" height="100%" fill="white" />\n\n' + \
     '<!-- fill jancode -->\n'
     meta_tail =  "\n\n</svg>"
--- python/jancode/property.py
+++ python/jancode/property.py
@@ -14,11 +14,13 @@ def set_property(number: str) -> None:
         jancode_number: str = number[1:]
         center: int = 5
         combi = combination(number[0])
+        jancode_type = "standard"
     else:
         center = 3
         jancode_number = number
         combi: str = (
             "OOOO"  # For short type, all strings on the left side are odd parity.
         )
+        jancode_type = "short"
 
-    return length, width, jancode_number, center, combi
+    return length, width, jancode_number, center, combi, jancode_type
